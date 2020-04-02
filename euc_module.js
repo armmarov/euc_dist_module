e10563d58e20860c89a04c3c81ab2f749c370b1d
Ammar <ammar.a@okwave.global>
2020-04-02 15:08:18 +0000

    Add size to the function

--- euc_module.js
+++ euc_module.js
@@ -17,21 +17,26 @@ const eucDistance = (a, b) => {
  * @desc Function to calculate euclidean distance by batch
  * @param a reference vector
  * @param b target array of vector
+ * @param s size of return array
  * @return sorted distance
  */
-const calcDistBatch = (a, b) => {
+const calcDistBatch = (a, b, s) => {
+
+	assert(a.length > 0)
 	assert(b.length > 0)
+	assert(s > 0)
 	
 	let calcArray = []
-	for (var i=0;i <b.length; i++) {
+	for (var i=0; i<b.length; i++) {
 		calcArray.push(eucDistance(a, b[i]))
 	}
 
 	var sortedIndex = Array.from(Array(calcArray.length).keys())
                   .sort((a, b) => calcArray[a] < calcArray[b] ? -1 : (calcArray[b] < calcArray[a]) | 0)
 	
+	let act_size = size => size <= sortedIndex.length ? size : sortedIndex.length
 	let ret_val = []
-	for (var i=0; i< sortedIndex.length; i++) {
+	for (var i=0; i<act_size(s); i++) {
 		ret_val.push({
 			coor: b[sortedIndex[i]],
 			dist: calcArray[sortedIndex[i]]
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var eucModule = require('./euc_module')
 
 var a = [90, 100];
-var b = [[20, 60], [30, 100], [40, 30]]
+var b = [[20, 60], 
+		 [30, 100], 
+		 [40, 30],
+		 [25, 65],
+		 [46, 120]]
 
-var resp = eucModule(a, b)
+var resp = eucModule(a, b, 3)
 
 console.log("Reference Vector: ", a)
 console.log("Target Vector: ", b)
